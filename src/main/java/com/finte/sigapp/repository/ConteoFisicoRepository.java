c8530dbac9e08fde8b45a53b5e8d3759d223db52
Freyre1927 <jfreyre17@gmail.com>
2026-07-28 10:40:08 -0500

    refactor: se elimina metodo en el repository de conteo fisico

--- src/main/java/com/finte/sigapp/repository/ConteoFisicoRepository.java
+++ src/main/java/com/finte/sigapp/repository/ConteoFisicoRepository.java
@@ -20,71 +20,57 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ConteoFisicoRepository {
 
-        private final ProcedureExecutor procedureExecutor;
+    private final ProcedureExecutor procedureExecutor;
 
-        /**
-         * Llama al procedimiento PL/SQL PKGCONTARBO.proGeneContFisi para generar el
-         * conteo físico.
-         */
-        public void generarConteoFisico(String empresa,
-                        String bodega,
-                        String bodegaLogica,
-                        String articulo,
-                        LocalDateTime fecha,
-                        String verificarExistencia) {
+    /**
+     * Llama al procedimiento PL/SQL PKGCONTARBO.proGeneContFisi para generar el
+     * conteo físico.
+     */
+    public void generarConteoFisico(String empresa,
+            String bodega,
+            String bodegaLogica,
+            String articulo,
+            LocalDateTime fecha,
+            String verificarExistencia) {
 
-                log.info("Llamando a proGeneContFisi - Empresa: {}, Bodega: {}, Bodega Lógica: {} Artículo: {}, Fecha: {}, Verificar Existencia: {}",
-                                empresa, bodega, bodegaLogica, articulo, fecha, verificarExistencia);
+        log.info(
+                "Llamando a proGeneContFisi - Empresa: {}, Bodega: {}, Bodega Lógica: {} Artículo: {}, Fecha: {}, Verificar Existencia: {}",
+                empresa, bodega, bodegaLogica, articulo, fecha, verificarExistencia);
 
-                // seteo parametros del procedimiento
-                List<ProcedureParam> params = List.of(
-                                new ProcedureParam("P_sbEmpr", ParameterMode.IN, String.class, empresa),
-                                new ProcedureParam("P_sbBode", ParameterMode.IN, String.class, bodega),
-                                new ProcedureParam("P_sbBolo", ParameterMode.IN, String.class, bodegaLogica),
-                                new ProcedureParam("P_sbArti", ParameterMode.IN, String.class, articulo),
-                                new ProcedureParam("P_dtFech", ParameterMode.IN, Date.class, fecha),
-                                new ProcedureParam("P_sbVaex", ParameterMode.IN, String.class, verificarExistencia));
-                procedureExecutor.ejecutarProcedimiento("PKGCONTARBO.proGeneContFisi",
-                                params);
-        }
+        // seteo parametros del procedimiento
+        List<ProcedureParam> params = List.of(
+                new ProcedureParam("P_sbEmpr", ParameterMode.IN, String.class, empresa),
+                new ProcedureParam("P_sbBode", ParameterMode.IN, String.class, bodega),
+                new ProcedureParam("P_sbBolo", ParameterMode.IN, String.class, bodegaLogica),
+                new ProcedureParam("P_sbArti", ParameterMode.IN, String.class, articulo),
+                new ProcedureParam("P_dtFech", ParameterMode.IN, Date.class, fecha),
+                new ProcedureParam("P_sbVaex", ParameterMode.IN, String.class, verificarExistencia));
+        procedureExecutor.ejecutarProcedimiento("PKGCONTARBO.proGeneContFisi",
+                params);
+    }
 
-        /**
-         * Invoca la función PL/SQL PKGCONTARBO.fun_ValidaBodega para verificar si la
-         * bodega
-         * ya se encuentra en conteo físico.
-         *
-         * @return true si la bodega ya está en conteo, false si no lo está.
-         */
-        public boolean validarBodegaEnConteo(String empresa, String bodega,
-                        String bodegaLogica, LocalDateTime fecha) {
+    /**
+     * Invoca la función PL/SQL PKGCONTARBO.fun_ValidaBodega para verificar si la
+     * bodega
+     * ya se encuentra en conteo físico.
+     *
+     * @return true si la bodega ya está en conteo, false si no lo está.
+     */
+    public boolean validarBodegaEnConteo(String empresa, String bodega,
+            String bodegaLogica, LocalDateTime fecha) {
 
-                log.info("Validando bodega en conteo - Empresa: {}, Bodega: {}, Bodega Lógica: {}, Fecha: {}",
-                                empresa, bodega, bodegaLogica, fecha);
+        log.info("Validando bodega en conteo - Empresa: {}, Bodega: {}, Bodega Lógica: {}, Fecha: {}",
+                empresa, bodega, bodegaLogica, fecha);
 
-                List<ProcedureParam> params = List.of(
-                                new ProcedureParam("sbCoabEmpr", ParameterMode.IN, String.class, empresa),
-                                new ProcedureParam("sbCoabBode", ParameterMode.IN, String.class, bodega),
-                                new ProcedureParam("sbCoabBolo", ParameterMode.IN, String.class, bodegaLogica),
-                                new ProcedureParam("dtCoabFech", ParameterMode.IN, LocalDateTime.class, fecha));
+        List<ProcedureParam> params = List.of(
+                new ProcedureParam("sbCoabEmpr", ParameterMode.IN, String.class, empresa),
+                new ProcedureParam("sbCoabBode", ParameterMode.IN, String.class, bodega),
+                new ProcedureParam("sbCoabBolo", ParameterMode.IN, String.class, bodegaLogica),
+                new ProcedureParam("dtCoabFech", ParameterMode.IN, LocalDateTime.class, fecha));
 
-                return procedureExecutor.ejecutarFuncion("PKGCONTARBO.fun_ValidaBodega",
-                                Boolean.class,
-                                params);
-        }
-
-        @Query(value = """
-                        SELECT
-                            DISTINCT a.COABBODE,
-                            b.BODEDESC
-                        FROM
-                            CONTARBO a
-                        JOIN
-                            BODEGA b ON b.bodecodi = a.COABBODE
-                        WHERE
-                            a.COABESTA = 'ac'
-                            AND a.COABDIEM = b.BODEEMPR
-                            AND b.BODEEMPR = :empresa
-                                            """, nativeQuery = true)
-        List<BodegasConteoPendientes> buscarBodegasConConteo(@Param("empresa") String empresa);
+        return procedureExecutor.ejecutarFuncion("PKGCONTARBO.fun_ValidaBodega",
+                Boolean.class,
+                params);
+    }
 
 }
