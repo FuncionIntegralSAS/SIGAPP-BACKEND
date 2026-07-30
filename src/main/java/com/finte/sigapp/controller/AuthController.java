ce9873d532ff7d6b59caf4bc654890fd5d5afda5
Freyre1927 <jfreyre17@gmail.com>
2026-07-30 14:05:54 -0500

    chore: se agrega la anotacion requestMapping al controrlador de autenticacion

--- src/main/java/com/finte/sigapp/controller/AuthController.java
+++ src/main/java/com/finte/sigapp/controller/AuthController.java
@@ -14,11 +14,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/api/v1/auth")
 public class AuthController {
 
     private final LoginService loginService;
